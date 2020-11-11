83c1b1d6aac776aaf88e0f0d94d5cf1ef03b7d34
SOvsyankin <sovsyankin@beeline.kz>
2020-11-11 16:44:50 +0600

    Initial commit

---
+++ src/test/java/com/sostud/photoapp/api/account/PhotoappAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sostud.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAccountManagementServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
